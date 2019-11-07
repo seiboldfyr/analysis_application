5eec932d806f9a2a3f43717b679aaf9521435d2d
seibold <claire@expesicor.com>
2019-11-07 11:25:42 -0700

    all data outputted to the data dictionary elastic beanstalk initiated reorder files

--- framework/abstract/abstract_processor.py
+++
@@ -1,7 +0,0 @@
-from framework.exception import MethodNotImplemented
-from framework.model.request.response import Response
-
-
-class AbstractProcessor:
-    def execute(self) -> Response:
-        raise MethodNotImplemented
