c2cc0a1852da9362f8bdfb1c068c37f969ac7648
seibold <claire@expesicor.com>
2019-11-27 01:21:58 -0700

    Speedy pandas dataframe built Integrated percent diff changes rfu graphs initialized

--- flaskr/graphing/graphs.py
+++ flaskr/graphing/graphs.py
@@ -1,17 +1,30 @@
 import numpy as np
 import seaborn
 import io
-import sys
 import base64
 import pandas as pd
 import time
+import sys
 import matplotlib
 matplotlib.use('Agg')
 
 from matplotlib import pyplot as plt
-from flaskr.model.helpers.functions import saveImage, get_unique_group
-from flaskr.model.helpers.buildfunctions import get_collection
+from flaskr.model.helpers.functions import saveImage, get_unique_group, get_unique
 from flaskr.database.dataset_models.repository import Repository
+
+
+def setGraphSettings():
+    params = {'legend.fontsize': 5,
+              'legend.loc': 'best',
+              'legend.framealpha': 0.5,
+              'figure.dpi': 250,
+              'legend.handlelength': .8,
+              'legend.markerscale': .4,
+              'legend.labelspacing': .4,
+              'font.size': 8}
+    plt.rcParams.update(params)
+    manualcolors = ["gray", "darkgreen", "cyan", "gold", "dodgerblue", "red", "lime", "magenta"]
+    seaborn.set_palette(manualcolors)
 
 
 class Grapher:
@@ -28,7 +41,7 @@ class Grapher:
         self.graph_urls = {}
 
     def execute(self):
-        self.setGraphSettings()
+        setGraphSettings()
 
         startpd = time.time()
         dataset_repository = Repository()
@@ -41,7 +54,6 @@ class Grapher:
                         value_vars=list(rfudf.columns)[-(len(df['cycle'])):],
                         var_name='time',
                         value_name='rfu')
-        print(rfudf.head(30))
         for inf in range(4):
             df['Inflection ' + str(inf)] = [x[inf] if len(x) == 4 else 0 for x in df['inflections']]
             df['Percent Diff ' + str(inf)] = [x[inf] if len(x) == 4 else 0 for x in df['percentdiffs']]
@@ -51,13 +63,21 @@ class Grapher:
                      value_name='value')
         print('pdtest: ', time.time() - startpd)
 
-        self.InflectionGraphByGroup(df[df['variable'].str.startswith('Inflection')])
-        # self.RFUIndividualGraphsByGroup(max(Groups), datadf)
-        # self.RFUAverageGraphsByGroup(max(Groups), datadf)
-        self.percentGraphs(df[df['variable'].str.startswith('Percent Diff ')])
+        startgraphing = time.time()
+        self.RFUIndividualGraphsByGroup(rfudf)
+        print('1', time.time() - startgraphing)
+        # self.RFUAverageGraphsByGroup(rfudf)
+        print('2', time.time() - startgraphing)
+        self.RFUAllGraphs(rfudf)
+        print('3', time.time() - startgraphing)
 
+        self.InflectionGraphByGroup(df[df['variable'].str.startswith('Inflection')])
+        print('4', time.time() - startgraphing)
         self.InflectionGraphsByNumber(df[df['variable'].str.startswith('Inflection')])
-        # self.RFUAllGraphs(datadf.sort_values(['index']))
+        print('5', time.time() - startgraphing)
+        self.percentGraphs(df[df['variable'].str.startswith('Percent Diff ')])
+        print('6', time.time() - startgraphing)
+
         print('graphs finished: ', time.time() - startpd)
         return self.graph_urls
 
@@ -79,15 +99,14 @@ class Grapher:
             self.saveimage(plt, 'Inflections_' + str(group))
 
     def InflectionGraphsByNumber(self, df):
-        df['triplicateIndex'] = int(df['group'].max())*(df['triplicate'] % 8)+df['group']
-        df['label'] = [x[:-2] for x in df['label']]
+        df.insert(0, 'triplicateIndex', int(df['group'].max())*(df['triplicate'] % 8)+df['group'])
         df = df.sort_values(by=['triplicateIndex', 'sample', 'triplicate', 'group'], ascending=True)
+        df['label'] = [x[:-2] for x in df['label']]
         numGroups = int(df['group'].max())
         xaxis = [i + 1 for i in range(numGroups)]
         xaxis = xaxis * int(len(df['sample']) / numGroups)
         for inf in range(4):
             gd = df[df['variable'] == "Inflection " + str(inf)]
-
             indplt = seaborn.swarmplot(x="triplicateIndex", y="value", hue="label", data=gd,
                                        marker='o', s=2.6, edgecolor='black', linewidth=.6)
             indplt.set(xticklabels=xaxis)
@@ -103,34 +122,40 @@ class Grapher:
             self.saveimage(plt, 'Inflection' + str(inf + 1))
 
 
-    def RFUIndividualGraphsByGroup(self, groups, df):
-        for group in range(1, groups+1):
-            seaborn.lineplot(x='time', y='value', hue='triplicate', units='index', estimator=None, data=df[df['group'] == group],
-                             linewidth=.7)
+    def RFUIndividualGraphsByGroup(self, df):
+        for group in range(1, int(df['group'].max())+1):
+            seaborn.lineplot(x='time', y='rfu', hue='triplicate', units='excelheader', estimator=None,
+                             data=df[df['group'] == group], linewidth=.7)
             plt.ylabel('RFU')
             plt.xlabel('Time (Min)')
-            saveImage(self, plt, 'Individuals_' + str(group))
+            self.saveimage(plt, 'Individuals_' + str(group))
 
-    def RFUAverageGraphsByGroup(self, groups, df):
-        for group in range(1, groups+1):
-            fig = plt.figure()
+    def RFUAverageGraphsByGroup(self, df):
+        for group in range(1, int(df['group'].max())+1):
             groupdf = df[df['group'] == group]
-            for triplicate in getUnique([welllabel['label'] for welllabel in self.data.values()]):
-                if int(triplicate[-1]) == group:
-                    subdf = groupdf[groupdf['triplicate'] == triplicate]
-                    seaborn.lineplot(subdf['time'], subdf.mean(1), label=triplicate, linewidth=.7)
+            seaborn.lineplot(x='time', y='rfu', label='label', data=groupdf, linewidth=.7)
+            # for triplicate in get_unique(groupdf['triplicate']):
+            #     subdf = groupdf[groupdf['triplicate'] == triplicate]
+            #     print(subdf.head(10))
+            #     print(subdf.groupby('label').mean(1))
+                # sys.exit()
+                # seaborn.lineplot(subdf['time'], subdf.mean(1), label=subdf['label'], linewidth=.7)
+            print(groupdf.head(20))
+            groupdf = groupdf.groupby('label')
+            print(groupdf.head(20))
+            print(groupdf.groupby('time').mean())
+            seaborn.lineplot(y='rfu', label='label', data=groupdf, linewidth=.7)
             plt.ylabel('RFU')
             plt.xlabel('Time (Min)')
-            saveImage(self, plt, 'Averages_' + str(group))
+            self.saveimage(plt, 'Averages_' + str(group))
 
     def RFUAllGraphs(self, df):
-        fig = plt.figure()
         manualcolors = ["gray", "darkgreen", "cyan", "gold", "dodgerblue", "red", "lime", "magenta"]
-        seaborn.lineplot(x='time', y='value', hue='group', units='index', estimator=None, data=df,
+        seaborn.lineplot(x='time', y='rfu', hue='group', units='excelheader', estimator=None, data=df,
                          palette=manualcolors[-np.max(df['group']):], linewidth=.7)  # hue='group', units='triplicate'
         plt.ylabel('RFU')
         plt.xlabel('Time (Min)')
-        saveImage(self, plt, 'Averages_All')
+        self.saveimage(plt, 'Averages_All')
 
     def percentGraphs(self, df):
         for group in range(1, int(df['group'].max())+1):
@@ -145,7 +170,6 @@ class Grapher:
             plt.ylabel('Percent Difference from Control')
             self.saveimage(plt, 'PercentDiff_' + str(group))
 
-
     def saveimage(self, plt, title):
         plt.title(title, fontsize=14)
         sio = io.BytesIO()
@@ -153,18 +177,6 @@ class Grapher:
         plt.close()
         self.graph_urls[title + '.png'] = base64.b64encode(sio.getvalue()).decode('utf-8').replace('\n', '')
 
-    def setGraphSettings(self):
-        params = {'legend.fontsize': 5,
-                  'legend.loc': 'best',
-                  'legend.framealpha': 0.5,
-                  'figure.dpi': 250,
-                  'legend.handlelength': .8,
-                  'legend.markerscale': .4,
-                  'legend.labelspacing': .4,
-                  'font.size': 8}
-        plt.rcParams.update(params)
-        manualcolors = ["gray", "darkgreen", "cyan", "gold", "dodgerblue", "red", "lime", "magenta"]
-        seaborn.set_palette(manualcolors)
 
 
 
