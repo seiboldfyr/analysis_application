c7451467d2e7179f28e3ed9ce1e2c575a66ee0e8
seibold <claire@expesicor.com>
2019-12-17 15:45:11 -0600

    initialized a writer inflections are created (vertically by well) averages are created neighbor differences calculated output is added to graph zip output Corrected sort on the inflection graphs

--- flaskr/filewriter/writer.py
+++ flaskr/filewriter/writer.py
@@ -19,7 +19,7 @@ class Writer:
         df = dataset.get_pd_well_collection()
         df = self.build_dataframe(df)
 
-        variablecolumns = [14 + n for n in range(12)]
+        variablecolumns = [14 + n for n in range(8)] #this will be 12 with inflection RFUs
         variablecolumns.insert(0, 4)
         for group in range(1, int(df['group'].max()) + 1):
             self.write_to_sheet('Inflections', df[(df['group'] == group)], variablecolumns, group)
@@ -32,51 +32,67 @@ class Writer:
             #TODO: control = df['label'].str.endswith('_0')
             control = gdf[gdf['triplicate'] == gdf['triplicate'].min()]
             for inf in range(4):
-                columns.append(26+inf)
+                columns.append(22+inf) #TODO: 26 when inflections
                 inf_label = 'Inflection ' + str(inf + 1)
-                gdf.insert(26+inf, 'Difference from control ' + str(inf + 1), adf[inf_label] - float(control[inf_label]))
+                gdf.insert(22+inf, 'Difference from control ' + str(inf + 1), adf[inf_label] - float(control[inf_label]))
             gdf = gdf.iloc[:, columns]
             gdf.to_excel(self.excelwriter, sheet_name='Averages', startrow=(group-1)*(gdf.shape[0]+3))
             self.excel_formatting('Averages', gdf)
 
         for group in range(1, int(adf['group'].max()) + 1):
-            columns = []
-            gdf = adf[(adf['group'] == group)]
             for inf in range(4):
+                columns = []
+                gdf = adf[(adf['group'] == group)]
                 inf_label = 'Inflection ' + str(inf + 1)
+                columns.append(6)
+                # TODO: can we add the sample number to the label?
                 for triplicateA in gdf['triplicate'].unique():
                     columns.append(len(gdf.columns))
                     rowA = gdf[gdf['triplicate'] == triplicateA]
-                    gdf.insert(len(gdf.columns), str(rowA['label']) + ' ' + inf_label, gdf[inf_label] - float(rowA[inf_label]))
-            self.write_to_sheet('Differences', gdf, columns, group)
-
+                    gdf.insert(len(gdf.columns), str(triplicateA) + ' ' + inf_label,
+                               [label - float(rowA[inf_label]) if triplicateB >= triplicateA else 'NA'
+                                for label, triplicateB in zip(gdf[inf_label], gdf['triplicate'])])
+                spacedifferencematrices = (len(columns)+4) * inf
+                self.write_to_sheet('Differences', gdf, columns, group, spacedifferencematrices)
         return Response(True, '')
 
     def build_dataframe(self, df):
         df = df[(df['is_valid'] == True)]
         for i in range(len(df['RFUs'][0])):
-            self.time.append(df['cycle'][0] * i / 60)
+            self.time.append(df['cycle'][0] * i)
+
         for inf in range(4):
             df['Inflection ' + str(inf + 1)] = [x[inf] if len(x) == 4 else 0 for x in df['inflections']]
-        for inf in range(4):
-            df['Inflection RFU ' + str(inf + 1)] = [x[inf] if len(x) == 4 else 0 for x in df['inflectionRFUs']]
+        # for inf in range(4):
+        #     df['Inflection RFU ' + str(inf + 1)] = [x[inf] if len(x) == 4 else 0 for x in df['inflectionRFUs']]
         for inf in range(4):
             df['Percent Diff ' + str(inf + 1)] = [x[inf] if len(x) == 4 else 0 for x in df['percentdiffs']]
         return df
 
-    def write_to_sheet(self, sheetname, df, columns, group):
+    def write_to_sheet(self, sheetname, df, columns, group, shiftcolumn=0):
         df = df.iloc[:, columns]
-        df.to_excel(self.excelwriter, sheet_name=sheetname, startrow=(group - 1) * (df.shape[0] + 3))
+        df.to_excel(self.excelwriter, sheet_name=sheetname,
+                    startrow=(group - 1) * (df.shape[0] + 3), startcol=shiftcolumn)
         self.excel_formatting(sheetname, df)
 
     def excel_formatting(self, sheetname, df):
         worksheet = self.excelwriter.sheets[sheetname]
-        maxlength = max(df.astype('str').applymap(lambda x: len(x)).max())
-        worksheet.set_column(1, len(df.columns), 2*maxlength/3)
+        for idx, column in enumerate(df.columns):
+            lengths = [len(x) for x in df.loc[:, column].astype('str')]
+            lengths.append(len(column))
+            maxlength = max(lengths)
+            worksheet.set_column(idx, idx+1, maxlength)
+        if sheetname != 'Inflections':
+            worksheet.set_column(0, 0, 30)
+        else:
+            worksheet.set_column(0, 0, 10)
 
     def build_averages(self, df):
         adf = pd.DataFrame(columns=df.columns.tolist())
         for triplicate in range(1, int(df['triplicate'].max()) + 1):
+            label = df.loc[0, 'label']
             gdf = df[(df['triplicate'] == triplicate)].groupby('label').mean()
+            gdf['label'] = label
             adf = pd.concat([adf, gdf], sort=False)
         return adf
+
