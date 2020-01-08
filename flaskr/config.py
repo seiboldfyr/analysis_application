4757e47adad802ebbf85b50c42d544b483e75fd5
seiboldfyr <55038129+seiboldfyr@users.noreply.github.com>
2020-01-08 12:22:34 -0700

    Delete config.py

--- flaskr/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SECRET_KEY = 'VqQjwX+APBaNsU1Q'
-DB_HOST = os.environ['DB_HOST']
-DB_NAME = os.environ['DB_NAME']
-DB_PORT = os.environ['DB_PORT']
-VERSION = os.environ['VERSION']
-APP_USERNAME = os.environ['APP_USERNAME']
-APP_PASSWORD = os.environ['APP_PASSWORD']
